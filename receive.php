d09c9c2900540171bf07fc8b192b89a9da1b4d51
Salmaan Saeed <salsaeed999@gmail.com>
2023-10-12 12:22:34 -0400

    removed commented code

--- receive.php
+++ receive.php
@@ -46,20 +46,4 @@
     echo 'Close connection...', PHP_EOL;
     $queue->cancel();
     $connection->disconnect();
-
-    /*
-    $channel->queue_declare('hello', false, false, false, false);
-
-    echo " [*] Waiting for messages. To exit press CTRL+C\n";
-
-    $callback = function ($msg) {
-        echo ' [x] Received ', $msg->body, "\n";
-    };
-
-    $channel->basic_consume('hello', '', false, true, false, false, $callback);
-
-    while ($channel-> is_open()) {
-        $channel->wait();
-    }
-    */
 ?>
