fb78e6a8c5f43ab1d20fb95d9cc9d4c28dd05b7b
Salmaan Saeed <salsaeed999@gmail.com>
2023-10-12 12:45:43 -0400

    configured the program to receive from the test user

--- receive.php
+++ receive.php
@@ -1,49 +1,52 @@
 #!/usr/bin/php
 <?php
-    // Connection
-    $connection = new AMQPConnection();
-    $connection->setHost('127.0.0.1');
-    $connection->setLogin('guest');
-    $connection->setPassword('guest');
-    $connection->connect();
+// Connection
+$connection = new AMQPConnection();
+$connection->setHost('127.0.0.1');
+$connection->setLogin('test');
+$connection->setPassword('test');
+$connection->setVhost('testHost');
+$connection->connect();
 
-    // Channel
-    $channel = new AMQPChannel($connection);
+// Channel
+$channel = new AMQPChannel($connection);
 
-    // Exchange
-    $exchange = new AMQPExchange($channel);
+// Exchange
+$exchange = new AMQPExchange($channel);
+$exchange->setName('testExchange');
 
-    $callback_func = function(AMQPEnvelope $message, AMQPQueue $q) use (&$max_consume) {
-        echo PHP_EOL, "----------", PHP_EOL;
-        echo " [X] Received ", $message->getBody(), PHP_EOL;
-        echo PHP_EOL, "----------", PHP_EOL;
+$callback_func = function(AMQPEnvelope $message, AMQPQueue $q) use (&$max_consume) {
+	echo PHP_EOL, "----------", PHP_EOL;
+	echo " [X] Received ", $message->getBody(), PHP_EOL;
+	echo PHP_EOL, "----------", PHP_EOL;
 
-        $q->nack($message->getDeliveryTag());
-        sleep(1);
-    };
+	$q->nack($message->getDeliveryTag());
+	sleep(1);
+};
 
-    // ----
+// ----
 
-    try {
-        $routing_key = 'hello';
+try {
+	$routing_key = 'hello';
 
-        $queue = new AMQPQueue($channel);
-        $queue->setName($routing_key);
-        $queue->setFlags(AMQP_NOPARAM);
-        $queue->declareQueue();
+	$queue = new AMQPQueue($channel);
 
-        echo ' [*] Waiting for messages. To exit press CTRL+C', PHP_EOL;
-        $queue->consume($callback_func);
-    }
-    catch(AMQPQueueException $ex) {
-        print_r($ex);
-    }
-    catch(Exception $ex) {
-        print_r($ex);
-    }
+	$queue->setName($routing_key);
+	$queue->setFlags(AMQP_NOPARAM);
+	$queue->declareQueue();
 
-    // Close Connections
-    echo 'Close connection...', PHP_EOL;
-    $queue->cancel();
-    $connection->disconnect();
+	echo ' [*] Waiting for messages. To exit press CTRL+C', PHP_EOL;
+	$queue->consume($callback_func);
+}
+catch(AMQPQueueException $ex) {
+	print_r($ex);
+}
+catch(Exception $ex) {
+	print_r($ex);
+}
+
+// Close Connections
+echo 'Close connection...', PHP_EOL;
+$queue->cancel();
+$connection->disconnect();
 ?>
